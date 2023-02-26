8d66dff959421e4ab946a1514d36dab700304050
Abdallak2202 <96998579+Abdallak2202@users.noreply.github.com>
2023-02-26 02:49:58 -0500

    Updated afterCreate method with slight differences

--- starlink/extensions/users-permissions/models/User.js
+++ starlink/extensions/users-permissions/models/User.js
@@ -1,11 +1,13 @@
 module.exports = {
-    lifecycles: {
-      async afterCreate(result, data) {
-        console.log('afterCreate method called');
-        const cart = await strapi.services.cart.create({
-          user: result._doc.id
-        });
-        await strapi.query('User', 'users-permissions').update({ id: result._doc.id }, { cart: cart.id });
-      }
+  lifecycles: {
+    async afterCreate(result, data) {
+      console.log("afterCreate method invoked")
+      const total = 0; // initial total value
+      const cart = await strapi.services.cart.create({
+        user: result._doc.id,
+        total: total
+      });
+      await strapi.query('User', 'users-permissions').update({ id: result._doc.id }, { cart: cart.id });
     }
-  };  
+  }
+};
