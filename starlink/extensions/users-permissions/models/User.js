7035a0e7704d8f0f28868085bef97abc6ec8870e
Abdallak2202 <96998579+Abdallak2202@users.noreply.github.com>
2023-02-26 01:55:24 -0500

    Updated afterCreate method

--- starlink/extensions/users-permissions/models/User.js
+++ starlink/extensions/users-permissions/models/User.js
@@ -1,10 +1,10 @@
 module.exports = {
     lifecycles: {
       async afterCreate(result, data) {
-        const cart = await strapi.services.cart.create({
-          user: result._doc.id
+        // Create a new cart for the newly created user
+        await strapi.services.cart.create({
+          user: result.id
         });
-        await strapi.query('User', 'users-permissions').update({ id: result._doc.id }, { cart: cart.id });
-      },
-    },
-  };
+      }
+    }
+  };  
